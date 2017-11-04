0a7b5bbae0fc7bc54ae489fc9ed395acdbd9bade
Qianzhu11 <2858583565@qq.com>
2017-11-03 22:16:35 -0400

    normal updates

--- lab8/data/palindromes.js
+++ lab8/data/palindromes.js
@@ -1,16 +1,20 @@
 let exportedMethods = {
     isPalindromes(string) {
         if (typeof string !== "string") throw "Must provide a string";
-        string = string.trim();
+        string = string.trim().replace(/\W+/g, " ").replace(/ /g,'').toLowerCase();
         let i = 0, j = string.length - 1;
         while(i < j) {
             if (string.charAt(i) !== string.charAt(j)) {
-                return "Oops, it's not a palindrome";
+                return "Oops,it's not a palindrome.";
+                break;
+            } else {
+                i++;
+                j--;
             }
-            i++;
-            j--;
         }
-        return "Congratulations, you find a palindrome";
+        if (j - i <= 1) {
+            return "Congratulations,you find a palindrome.";
+        }
     }
 }
 
--- lab8/routes/palindromes.js
+++ lab8/routes/palindromes.js
@@ -6,3 +6,7 @@ const palindrome = data.palindrome;
 router.get("/static", (req, res) => {
     res.render("palindrome/static", {});
 });
+
+router.get("/server", (req, res) => {
+    res.render("palindrome/server", {});
+});
